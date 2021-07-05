726300f8cdad9930cc9e2fd4038f4f89a9762967
KeithTohx <keith2703@icloud.com>
2021-07-05 17:05:24 +0800

    added input boxes for BMI calculator

--- app/src/main/java/sg/edu/np/mad/madfit/BmiCalculatorActivity.java
+++ app/src/main/java/sg/edu/np/mad/madfit/BmiCalculatorActivity.java
@@ -2,13 +2,26 @@ package sg.edu.np.mad.madfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BmiCalculatorActivity extends AppCompatActivity {
+    Button cancelBtn, calBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
+
+        cancelBtn = findViewById(R.id.cancel_btn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BmiCalculatorActivity.this, BmiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
