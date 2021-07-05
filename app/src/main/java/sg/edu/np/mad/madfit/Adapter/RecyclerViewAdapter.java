03b5ffd8ae58972d6d29c3a0912db38ec2a11433
lim44104 <s10207455@connect.np.edu.sg>
2021-07-05 10:13:00 +0800

    Add exercise list recycler view

---
+++ app/src/main/java/sg/edu/np/mad/madfit/Adapter/RecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package sg.edu.np.mad.madfit.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import sg.edu.np.mad.madfit.Interface.ItemClickListener;
+import sg.edu.np.mad.madfit.ListExercises;
+import sg.edu.np.mad.madfit.Model.Exercise;
+import sg.edu.np.mad.madfit.R;
+
+class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public ImageView image;
+    public TextView text;
+
+    private ItemClickListener itemClickListener;
+
+    public RecyclerViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+        image = (ImageView)itemView.findViewById(R.id.ex_img);
+        text = (TextView) itemView.findViewById(R.id.ex_name);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition());
+    }
+}
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
+
+    private List<Exercise> exerciseList;
+    private Context context;
+
+    public RecyclerViewAdapter(List<Exercise> exerciseList, Context context) {
+        this.exerciseList = exerciseList;
+        this.context = context;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.item_exercise,parent,false);
+
+        return new RecyclerViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerViewHolder holder, int position) {
+        holder.image.setImageResource(exerciseList.get(position).getImage_id());
+        holder.text.setText(exerciseList.get(position).getName());
+
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                //Intent intent
+                Toast.makeText(context, "Click to " + exerciseList.get(position).getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return exerciseList.size();
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/madfit/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package sg.edu.np.mad.madfit.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick (View view,int position);
+}
