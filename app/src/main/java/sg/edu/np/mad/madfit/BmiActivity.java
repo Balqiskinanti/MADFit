dd2691471507fd8175f999600980647fc9026e52
KeithTohx <keith2703@icloud.com>
2021-07-05 15:39:37 +0800

    added BMI Calculator page

--- app/src/main/java/sg/edu/np/mad/madfit/BmiActivity.java
+++ app/src/main/java/sg/edu/np/mad/madfit/BmiActivity.java
@@ -6,16 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BmiActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
+    Button newCalButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        newCalButton = findViewById(R.id.calBMI_button);
+        newCalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BmiActivity.this, BmiCalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
 
         navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setSelectedItemId(R.id.nav_home);
