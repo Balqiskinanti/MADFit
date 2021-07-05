05f6c160bcb7144cf5df261ad7ab218e16d588e7
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-07-06 04:07:49 +0800

    Add music activity

---
+++ app/src/main/java/sg/edu/np/mad/madfit/MusicActivity.java
@@ -0,0 +1,60 @@
+package sg.edu.np.mad.madfit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MusicActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+
+        TextView chillRun_btn = findViewById(R.id.chillRun_btn);
+        chillRun_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(MusicActivity.this, MusicPlayerActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setSelectedItemId(R.id.nav_home);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+
+                        break;
+
+                    case R.id.nav_workout:
+                        Intent intent1 = new Intent(MusicActivity.this,WorkoutActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.nav_food:
+                        Intent intent2 = new Intent(MusicActivity.this,FoodActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                }
+                return false;
+            }
+        });
+    }
+}
