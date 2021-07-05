83fb83f1693a101db2f187cb8aea77dbae1f9625
KeithTohx <keith2703@icloud.com>
2021-07-05 17:14:23 +0800

    edit activity to find input and buttons

--- app/src/main/java/sg/edu/np/mad/madfit/BmiCalculatorActivity.java
+++ app/src/main/java/sg/edu/np/mad/madfit/BmiCalculatorActivity.java
@@ -6,14 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class BmiCalculatorActivity extends AppCompatActivity {
+    EditText myHeightInput, myWeightInput;
     Button cancelBtn, calBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
+
+        myHeightInput = findViewById(R.id.height_input);
+        myWeightInput = findViewById(R.id.weight_input);
+
+        calBtn = findViewById(R.id.cal_btn);
+        calBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //calculate BMI, check Status & Alert Dialog
+
+            }
+        });
 
         cancelBtn = findViewById(R.id.cancel_btn);
         cancelBtn.setOnClickListener(new View.OnClickListener() {
