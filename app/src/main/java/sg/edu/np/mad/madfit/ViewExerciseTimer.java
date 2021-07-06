aab23bf19e50a38ac12961c3dab4a9c7278cc79c
lim44104 <s10207455@connect.np.edu.sg>
2021-07-06 17:08:13 +0800

    Add exercise finished page layout

--- app/src/main/java/sg/edu/np/mad/madfit/ViewExerciseTimer.java
+++ app/src/main/java/sg/edu/np/mad/madfit/ViewExerciseTimer.java
@@ -2,13 +2,27 @@ package sg.edu.np.mad.madfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewExerciseTimer extends AppCompatActivity {
+
+    Button btnFinished;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_exercise_timer);
+
+        btnFinished = (Button)findViewById(R.id.wFinishBtn);
+        btnFinished.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewExerciseTimer.this,ExerciseFinished.class);
+                startActivity(intent);
+            }
+        });
     }
 }
