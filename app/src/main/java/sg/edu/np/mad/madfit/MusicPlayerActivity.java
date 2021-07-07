d511e36541e687f12f77688b1bba19f1b726f95b
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-07-08 01:33:18 +0800

    Add more music player

--- app/src/main/java/sg/edu/np/mad/madfit/MusicPlayerActivity.java
+++ app/src/main/java/sg/edu/np/mad/madfit/MusicPlayerActivity.java
@@ -14,9 +14,20 @@ public class MusicPlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music_player);
 
         // load music from https://www.mixcloud.com/
-        WebView webView = findViewById(R.id.webView);
+        WebView webView = findViewById(R.id.webView1);
         String dataUrl = "<iframe width=\"100%\" height=\"400\" src=\"https://www.mixcloud.com/widget/iframe/?dark=1&feed=%2Fakeymusicfactory%2Fall-sza-selection%2F\" frameborder=\"0\" ></iframe>";
+        loadMusicPlayer(webView, dataUrl);
 
+        WebView webView2 = findViewById(R.id.webView2);
+        String dataUrl2 = "<iframe width=\"100%\" height=\"400\" src=\"https://www.mixcloud.com/widget/iframe/?feed=%2FDJ_Mattrixx%2Fdrake-scorpion-album-mix%2F\" frameborder=\"0\" ></iframe>";
+        loadMusicPlayer(webView2, dataUrl2);
+
+        WebView webView3 = findViewById(R.id.webView3);
+        String dataUrl3 = "<iframe width=\"100%\" height=\"400\" src=\"https://www.mixcloud.com/widget/iframe/?light=1&feed=%2FDJNUT_N_NICE%2Ftravis-scott-x-don-toliver-astro-in-hell%2F\" frameborder=\"0\" ></iframe>";
+        loadMusicPlayer(webView3, dataUrl3);
+    }
+
+    private void loadMusicPlayer(WebView webView, String dataUrl){
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
