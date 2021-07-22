04a0d029973840d889d2d508c75c062a0f9ca18c
KeithTohx <keith2703@icloud.com>
2021-07-22 15:30:06 +0800

    added calorie dashboard

--- app/src/main/java/sg/edu/np/mad/madfit/FoodActivity.java
+++ app/src/main/java/sg/edu/np/mad/madfit/FoodActivity.java
@@ -3,7 +3,11 @@ package sg.edu.np.mad.madfit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,19 +15,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class FoodActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+public class FoodActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
+    EditText dailyCalInput;
+    TextView calNeeded;
+    String myCal;
+    Button setTargetBtn, calCounterBtn;
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
-        TextView title = (TextView) findViewById(R.id.activityTitle2);
-        title.setText("This is food activity");
+        dailyCalInput = findViewById(R.id.etDailyCalTarget);
+        calNeeded = findViewById(R.id.caloriesNeeded);
 
-        // Bottom navigation
+        /*
+        Go to Calorie Counter page
+         */
+        calCounterBtn = findViewById(R.id.calCounter_btn);
+        calCounterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FoodActivity.this, CalorieCounter.class);
+                startActivity(intent);
+            }
+        });
+
+        /*
+        Display Date in dashboard
+         */
+        TextView currentDT = findViewById(R.id.currentDT);
+        Calendar c = Calendar.getInstance();
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH);
+        int year = c.get(Calendar.YEAR);
+        int dayWeek = c.get(Calendar.DAY_OF_WEEK);
+        List dayWkList = initDayWeek();
+        String date = dayWkList.get(dayWeek-1) + " " + day + "/" + (month+1) + "/" + year;
+        currentDT.setText(date);
+
+        /*
+        Bottom navigation
+        */
         navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setSelectedItemId(R.id.nav_food);
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -48,5 +86,20 @@ public class FoodActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    /*
+    initialise list of Days in a Week
+     */
+    private List initDayWeek(){
+        List dayWkList = new ArrayList();
+        dayWkList.add("Sunday");
+        dayWkList.add("Monday");
+        dayWkList.add("Tuesday");
+        dayWkList.add("Wednesday");
+        dayWkList.add("Thursday");
+        dayWkList.add("Friday");
+        dayWkList.add("Saturday");
+        return dayWkList;
     }
 }
