55defa7c1e19c4fb5daa53608fbe35942780b7dd
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-07-26 05:06:17 +0800

    Add validation, check if network available

--- app/src/main/java/sg/edu/np/mad/madfit/MailActivity.java
+++ app/src/main/java/sg/edu/np/mad/madfit/MailActivity.java
@@ -1,15 +1,21 @@
 package sg.edu.np.mad.madfit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,11 +29,22 @@ public class MailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
 
+        mEmail = findViewById(R.id.mEmail);
         sendEmail_btn = findViewById(R.id.sendEmail_Btn);
         sendEmail_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendMail();
+                if(isNetworkAvailable()){
+                    if(mEmail.getText().toString().equals("")){
+                        Toast.makeText(MailActivity.this, "Please provide your email", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        sendMail();
+                    }
+                }
+                else{
+                    noInternetAlert();
+                }
             }
         });
 
@@ -69,5 +86,25 @@ public class MailActivity extends AppCompatActivity {
 
         JavaMailAPI javaMailAPI = new JavaMailAPI(this, mail, subject, message);
         javaMailAPI.execute();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+    private void noInternetAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MailActivity.this);
+        builder.setMessage("You are offline. Please check your internet connection to use this feature.");
+        builder.setTitle("No Internet Connection");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
