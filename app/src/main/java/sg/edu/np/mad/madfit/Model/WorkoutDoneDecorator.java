e502d0162fc693245c0518cb8739568a200a39c2
lim44104 <s10207455@connect.np.edu.sg>
2021-07-26 21:33:01 +0800

    Complete Workout Calender

---
+++ app/src/main/java/sg/edu/np/mad/madfit/Model/WorkoutDoneDecorator.java
@@ -0,0 +1,31 @@
+package sg.edu.np.mad.madfit.Model;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+import java.util.HashSet;
+
+public class WorkoutDoneDecorator implements DayViewDecorator {
+
+    HashSet<CalendarDay> list;
+    ColorDrawable colorDrawable;
+
+    public WorkoutDoneDecorator(HashSet<CalendarDay> list) {
+        this.list = list;
+        colorDrawable = new ColorDrawable(Color.parseColor("#5868E0"));
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay calendarDay) {
+        return list.contains(calendarDay);
+    }
+
+    @Override
+    public void decorate(DayViewFacade dayViewFacade) {
+        dayViewFacade.setBackgroundDrawable(colorDrawable);
+    }
+}
--- app/src/main/java/sg/edu/np/mad/madfit/WorkoutCalendar.java
+++ app/src/main/java/sg/edu/np/mad/madfit/WorkoutCalendar.java
@@ -8,15 +8,38 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+import sg.edu.np.mad.madfit.Model.WorkoutDoneDecorator;
 
 public class WorkoutCalendar extends AppCompatActivity {
 
     BottomNavigationView navigationView;
+    MaterialCalendarView materialCalendarView;
+    HashSet<CalendarDay> list = new HashSet<>();
+    MADFitDBHandler madFitDBHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_calendar);
+
+        madFitDBHandler = new MADFitDBHandler(this);
+        materialCalendarView = (MaterialCalendarView)findViewById(R.id.calendar);
+
+        List<String> workoutDay = madFitDBHandler.getWorkoutDays();
+        HashSet<CalendarDay> convertedList = new HashSet<>();
+        for(String value:workoutDay){
+            convertedList.add(CalendarDay.from(new Date(Long.parseLong(value))));
+        }
+
+        materialCalendarView.addDecorator(new WorkoutDoneDecorator(convertedList));
 
         // Bottom navigation
         navigationView = findViewById(R.id.bottom_navigation);
